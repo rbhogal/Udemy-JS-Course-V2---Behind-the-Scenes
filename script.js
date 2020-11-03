0b6758181439d57eaf789a278656b800ae1bb8be
rbhogal <rbhogal23gmail.com>
2020-11-03 10:58:46 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict';
+/* 
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+    }
+
+    // console.log(add(2,3));
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Rohit';
+calcAge(1991);
+ */
+
+ // variables
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+ var me = 'Rohit';
+ let job = 'teacher';
+ const year = 1991; 
+
+ // funcions
+console.log(addDecl(2,3));
+console.log(addExpr(2,3));
+console.log(addArrow(2,3));
+
+function addDecl(a,b) {
+    return a + b;
+}
+
+var addExpr = function(a,b) {
+    return a + b;
+}
+
+var addArrow = (a,b) => a + b;
+
+
+// Example
+
