58c695d52d82fb9cd828da6a97412f4c31684c41
rbhogal <rbhogal23gmail.com>
2020-11-03 11:12:31 -0800

    finish

--- script.js
+++ script.js
@@ -39,8 +39,8 @@ calcAge(1991);
 
  // funcions
 console.log(addDecl(2,3));
-console.log(addExpr(2,3));
-console.log(addArrow(2,3));
+// console.log(addExpr(2,3));
+// console.log(addArrow(2,3));
 
 function addDecl(a,b) {
     return a + b;
@@ -54,4 +54,20 @@ var addArrow = (a,b) => a + b;
 
 
 // Example
+
+if(!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(x === window.y);
+console.log(x === window.z);
 
